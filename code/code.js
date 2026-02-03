7996c77f93f9778f340c36c254c88592596d2503
TobiasHoffmann52 <88829745+TobiasHoffmann52@users.noreply.github.com>
2026-02-03 23:50:42 +0100

    start

---
+++ code/code.js
@@ -0,0 +1,15 @@
+function preload() {
+  img = loadImage ('/assets/billede.jpg');
+}
+
+function setup() {
+  createCanvas(1000, 1000);
+  background(255, 234, 209);
+  //image(img,0,0,500,500);
+  fill(255);
+}
+
+
+function draw() {
+  
+}
